b88a9c6c85e62893f75e84f3eb93f50e1118149f
olamide folayan <holladred@gmail.com>
2025-03-18 17:41:45 +0100

    initialize Firebase hosting configuration and generate default connector SDK

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'nacos',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'nacos',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
